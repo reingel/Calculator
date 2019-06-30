35fa0438e7890055c519419b2093ac3bcd021343
SoonkyuJeong <reingel@gmail.com>
2019-06-30 15:46:24 +0900

    Initial Commit

---
+++ Calculator/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Soonkyu Jeong on 2019/06/30.
+//  Copyright © 2019 Soonkyu Jeong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
